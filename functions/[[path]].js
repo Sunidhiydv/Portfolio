6e890fb19278b0bab45eb2246da9a9c106cf8f41
Neel jain <nj6097114@gmail.com>
2025-04-29 13:09:35 +0530

    Revert "Vercel config addtion of remix"
    This reverts commit fe05de3d34150e86743cdcfd5d4dc1fc6de412bf.

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,11 +1,5 @@
-import * as build from "@remix-run/dev/server-build";
-import { createRequestHandler } from "@remix-run/cloudflare";
+import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages';
 
-const handleRequest = createRequestHandler({
-  build,
-  mode: process.env.NODE_ENV,
-});
+import * as build from '../build/server';
 
-export function onRequest(context) {
-  return handleRequest(context.request);
-}
+export const onRequest = createPagesFunctionHandler({ build });
